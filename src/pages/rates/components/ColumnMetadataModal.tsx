e1544d3753d70ff4f24289aadd83b7d4d6da7f24
Shahvinit3113 <shahvinit315@gmail.com>
2026-06-27 10:15:59 +0530

    Changes in plan

--- src/pages/rates/components/ColumnMetadataModal.tsx
+++ src/pages/rates/components/ColumnMetadataModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 import { Modal } from '@/components/common';
 import { Button } from '@/components/ui/Button';
